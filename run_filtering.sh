60445a7e11be158632754ceb2f0b884236664e88
go-akshit <akshit@stanford.edu>
2024-05-29 22:23:11 -0700

    filtering_try

--- run_filtering.sh
+++ run_filtering.sh
@@ -6,4 +6,4 @@
 #SBATCH --time=24:00:00
 #SBATCH --output=filtering%j.out
 #SBATCH --error=filtering%j.err
-wget --timeout=5 -i positive_url_sample1.txt --warc-file=subsampled_positive_urls1.warc -O /dev/null
+python3 cs336-data/cs336_data/cc_filtering.py
